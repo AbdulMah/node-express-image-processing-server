f197235eb3a200c5dfbfd59a2ee1b7e973653672
Abdoulkarim <amahamn@gmail.com>
2021-11-15 03:57:17 +0100

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -29,14 +29,14 @@ const upload = multer({
     storage,
 });
 
-router.post('/upload', upload.single('photo'),  (request, response) => {
+router.post('/upload', upload.single('photo'),  async(request, response) => {
     if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
     
-  /**  try {
+    try {
         await imageProcessor(request.file.filename)
     } catch{
         
-    }; */
+    }; 
     return response.status(201).json({success: true});
 });
 
